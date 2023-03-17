e2179ed914d12eddaedd4c1f785b7ad628a934b5
thoainguyen2007 <125574535+thoainguyen2007@users.noreply.github.com>
2023-03-17 20:01:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,7 @@ import streamlit as st
 import sklearn
 import pickle
 st.title("REVENUE PREDICTION")
-t = st.number_input('Input Temperature')
-filename = 'model.pickle'
 
-# pickle.dump(model, open(filename, "wb"))
-# model = pickle.load(open(filename, "rb"))
 if st.button('Predict'):
   st.caption('Revenue predicttion')
-  st.success(model)
+  st.success()
